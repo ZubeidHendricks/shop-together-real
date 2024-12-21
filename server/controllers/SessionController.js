8310cfad47de31477f465c3adf4d627d2b639839
Zubeid Hendricks <zubeid.hendricks@gmail.com>
2024-12-21 21:16:14 +0200

    Fix SessionController with proper function declarations

--- server/controllers/SessionController.js
+++ server/controllers/SessionController.js
@@ -1,7 +1,7 @@
 const Session = require('../models/Session');
 const Cart = require('../models/Cart');
 
-const createSession = async (req, res) => {
+async function createSession(req, res) {
   try {
     const session = await Session.create({
       host: req.user._id,
@@ -19,32 +19,23 @@ const createSession = async (req, res) => {
     session.cartId = cart._id;
     await session.save();
 
-    const populatedSession = await Session.findById(session._id)
-      .populate('participants.user', 'name email')
-      .populate('cartId');
-
-    res.status(201).json(populatedSession);
+    res.status(201).json(session);
   } catch (error) {
-    console.error('Session creation error:', error);
     res.status(500).json({ error: error.message });
   }
-};
+}
 
-const getSessionDetails = async (req, res) => {
+async function getSessionDetails(req, res) {
   try {
-    const session = await Session.findById(req.params.sessionId)
-      .populate('participants.user', 'name email')
-      .populate('cartId');
-
+    const session = await Session.findById(req.params.sessionId);
     if (!session) {
       return res.status(404).json({ error: 'Session not found' });
     }
-
     res.json(session);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-};
+}
 
 module.exports = {
   createSession,
